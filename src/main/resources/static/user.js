6f8308724435509ed3d2ac307856cbe0a5111e11
elena1989 <elena.shilova89@gmail.com>
2022-11-18 18:10:01 +0300

    корректировки

--- src/main/resources/static/user.js
+++ src/main/resources/static/user.js
@@ -7,15 +7,11 @@ $(async function () {
 
 async function loadUser() {
     fetch("http://localhost:8088/api/user")
-        .then(r => r.json()) //читаем ответ в JSON
+        .then(r => r.json())
         .then(data => {
-            // Подставляем полученную информацию в navbar
             $('#navUsername').append(data.username);
-            //Получаем роли без подстроки ROLE_
             let roles = data.roles.map(role => " " + role.name.substring(5));
             $('#navRoles').append(roles);
-
-            //Заполняем таблицу полученными данными
             let user = `$(
             <tr>
                 <td>${data.id}</td>
@@ -24,10 +20,8 @@ async function loadUser() {
                 <td>${data.age}</td>
                 <td>${data.username}</td>
                 <td>${roles}</td>)`;
-            //Вставляем все в тело таблицы
             $('#userPanelBody').append(user);
         })
-        //Выводим ошибку если что-то пошло не так
         .catch((error) => {
             alert(error);
         });
